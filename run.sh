d58c59fbabf4347a3481bdde5a2fd9566f09086f
Barnaby Keene <hello@southcla.ws>
2018-09-23 15:49:13 +0100

    updated run to init db

--- run.sh
+++ run.sh
@@ -10,6 +10,12 @@ FORUM_SMTP_USER=${FORUM_SMTP_USER:-"user@example.com"}
 FORUM_SMTP_PASSWORD=${FORUM_SMTP_PASSWORD:-password}
 FORUM_IS_DEV=${FORUM_IS_DEV:-false}
 
+# set up as if we're dev
+# this forces the admin account to be created
+./setup_nimforum --dev
+mv nimforum-dev.db shared/nimforum.db
+
+# then overwrite the config with our actual data
 cat >forum.json <<EOF
 {
     "name": "$FORUM_NAME",
